5eabc5656a98b29b290c762e6447876d4672206a
Stephen Gaito <stephen@perceptisys.co.uk>
2023-04-05 21:48:07 +0100

    initial commit of old existing project

---
+++ src/extension.ts
@@ -0,0 +1,91 @@
+
+import { sync as commandExists } from 'command-exists'
+import * as vscode from 'vscode'
+import {
+  ServerOptions,
+  LanguageClientOptions,
+  WorkDoneProgressCancelNotification
+} from 'vscode-languageclient/node'
+import { BuildStatus, TexLanguageClient } from './client'
+import { CONTEXT_FILE, CONTEXT_UNTITLED } from './selectors'
+import { Messages, StatusIcon } from './view'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const thingsToDispose : Array<{ dispose() : any }> = [];
+let client : TexLanguageClient ;
+
+export async function activate(_vsContext: vscode.ExtensionContext) {
+
+  const serverName = 'context-langserver'
+
+  if (! commandExists(serverName)) {
+    throw new Error(Messages.SERVER_NOT_FOUND)
+  }
+
+  const serverOptions: ServerOptions = {
+    run: { command: serverName },
+    debug: { command: serverName, args: ['-v'] }
+  }
+
+  const clientOptions: LanguageClientOptions =  {
+    documentSelector: [ CONTEXT_FILE, CONTEXT_UNTITLED ],
+    outputChannelName: 'ConTeXt'
+  }
+
+  client = new TexLanguageClient(serverName, serverOptions, clientOptions)
+
+  const icon = new StatusIcon()
+
+  thingsToDispose.push(
+    vscode.commands.registerTextEditorCommand('context.build', editor =>
+      build(editor, client),
+    ),
+    vscode.commands.registerCommand('context.build.cancel', () =>
+      client.sendNotification(WorkDoneProgressCancelNotification.type, {
+        token: 'context-build-*',
+      }),
+    ),
+    client.onDidChangeState(({ newState }) => {
+      icon.update(newState)
+    }),
+    client,
+    icon,
+  )
+
+  client.start()
+}
+
+export async function deactivate() {
+  if (client) {
+    client.stop();
+  }
+  thingsToDispose.forEach( (aDisposableThing) => 
+    aDisposableThing.dispose()
+  )
+}
+
+async function build(
+  { document }: vscode.TextEditor,
+  client: TexLanguageClient,
+): Promise<void> {
+  if (
+    vscode.languages.match([CONTEXT_FILE], document) <=
+      0 ||
+    (document.isDirty && !(await document.save()))
+  ) {
+    return
+  }
+
+  const result = await client.build(document)
+  switch (result.status) {
+    case BuildStatus.Success:
+    case BuildStatus.Cancelled:
+      break
+    case BuildStatus.Error:
+      vscode.window.showErrorMessage(Messages.BUILD_ERROR)
+      break
+    case BuildStatus.Failure:
+      vscode.window.showErrorMessage(Messages.BUILD_FAILURE)
+      break
+  }
+}
---
+++ src/selectors.ts
@@ -0,0 +1,9 @@
+export const CONTEXT_FILE = {
+  language: 'context',
+  scheme: 'file',
+}
+
+export const CONTEXT_UNTITLED = {
+  language: 'context',
+  scheme: 'untitled',
+}
